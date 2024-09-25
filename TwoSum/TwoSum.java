9ffdb0eb8418f1cc05d019f0502896241338c854
JuliaKolano <juliakolano02@gmail.com>
2024-09-25 14:54:57 +0100

    two pass hash table solution for the two sum problem

--- TwoSum/TwoSum.java
+++ TwoSum/TwoSum.java
@@ -1,6 +1,8 @@
 package TwoSum;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target
 // You may assume that each input would have exactly one solution, and you may not use the same element twice
@@ -12,7 +14,7 @@ class TwoSum {
     // Time Complexity: O(n^2)
     // Space Complexity: O(1)
 
-    public static int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum1(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[j] == target - nums[i]) {
@@ -24,14 +26,42 @@ class TwoSum {
         return new int[] {};
     }
 
-    
-    //Testing Cases
+
+    // Two-pass Hash Table:
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+
+    public static int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        // In case there is no solution, return an empty array
+        return new int[] {};
+    }
+
+
+    // Testing Cases
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(twoSum(new int[] {2, 7, 11, 15}, 9))); // Answer: [0, 1]
-        System.out.println(Arrays.toString(twoSum(new int[] {3, 2, 4}, 6))); // Answer: [1, 2]
-        System.out.println(Arrays.toString(twoSum(new int[] {3, 3}, 6))); // Answer: [0, 1]
+        // Brute Force
+        System.out.println("Brute Force:");
+        System.out.println(Arrays.toString(twoSum1(new int[] {2, 7, 11, 15}, 9))); // Answer: [0, 1]
+        System.out.println(Arrays.toString(twoSum1(new int[] {3, 2, 4}, 6))); // Answer: [1, 2]
+        System.out.println(Arrays.toString(twoSum1(new int[] {3, 3}, 6))); // Answer: [0, 1]
         
+
+        // Two-pass Hash Table
+        System.out.println("Two-pass Hash Table:");
+        System.out.println(Arrays.toString(twoSum2(new int[] {2, 7, 11, 15}, 9))); // Answer: [0, 1]
+        System.out.println(Arrays.toString(twoSum2(new int[] {3, 2, 4}, 6))); // Answer: [1, 2]
+        System.out.println(Arrays.toString(twoSum2(new int[] {3, 3}, 6))); // Answer: [0, 1]
     }
 
 }
