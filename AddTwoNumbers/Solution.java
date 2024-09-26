df6ea3c976864777cec9221474fee90ef9608c62
JuliaKolano <juliakolano02@gmail.com>
2024-09-26 12:16:42 +0100

    added solution to add two numbers

--- AddTwoNumbers/Solution.java
+++ AddTwoNumbers/Solution.java
@@ -4,4 +4,33 @@ package AddTwoNumbers;
 // and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 // You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
-public class Solution {}
+public class Solution {
+
+    // Time Complexity: O(max(m,n))
+    // Space Complexity: O(1)
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            int sum = carry + x + y;
+
+            carry = sum / 10;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+
+        }
+
+        return dummyHead.next;
+    }
+
+}
