f86421a6f0b74df4076458a552a8bc71f2f3281b
JuliaKolano <juliakolano02@gmail.com>
2024-09-29 16:09:32 +0100

    added the merge sort solution to the median of two sorted arrays problem

---
+++ MedianOfTwoSortedArrays/MergeSort.java
@@ -0,0 +1,43 @@
+package MedianOfTwoSortedArrays;
+
+// Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+// The overall run time complexity should be O(log(m+n)).
+
+public class MergeSort {
+    
+    // Time Complexity: O()
+    // Space Complexity: O()
+
+    private int p1 = 0, p2 = 0;
+
+    // Get the smaller value between nums1[p1] and nums2[p2] and move the pointer forwards
+
+    private int getMin(int[] nums1, int[] nums2) {
+
+        if (p1 < nums1.length && p2 < nums2.length) {
+            return nums1[p1] < nums2[p2] ? nums1[p1++] : nums2[p2++];
+        } else if (p1 < nums1.length) {
+            return nums1[p1++];
+        } else if (p2 < nums2.length) {
+            return nums2[p2++];
+        }
+
+        return -1;
+
+    }
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        int m = nums1.length, n = nums2.length;
+
+        if ((m + n) % 2 == 0) {
+
+            for (int i = 0; i < (m + n) / 2 - 1; i++ ) {
+                
+            }
+
+        }
+
+    }
+
+}
