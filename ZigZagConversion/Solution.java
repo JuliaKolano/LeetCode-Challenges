352b91ab9684034c234341a8b5014d530e8fd509
JuliaKolano <juliakolano02@gmail.com>
2024-10-02 14:58:32 +0100

    updated the solution for the zigzag conversion problem

--- ZigZagConversion/Solution.java
+++ ZigZagConversion/Solution.java
@@ -10,4 +10,38 @@ package ZigZagConversion;
 
 public class Solution {
 
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+
+    public String convert(String s, int numRows) {
+
+        char[] c = s.toCharArray();
+        int length = c.length;
+        StringBuffer[] sb = new StringBuffer[numRows];
+
+        for (int i = 0; i < sb.length; i++) {
+            sb[i] = new StringBuffer();
+        }
+
+        int i = 0;
+
+        while (i < length) {
+
+            // Vettically down
+            for (int index = 0; index < numRows && i < length; index++) {
+                sb[index].append(c[i++]);
+            }
+
+            // Obliquely up
+            for (int index = numRows - 2; index >= 1 && i < length; index--) {
+                sb[index].append(c[i++]);
+            }
+        }
+
+        for (int index = 1; index < sb.length; index++) {
+            sb[0].append(sb[index]);
+        }
+
+        return sb[0].toString();
+    }
 }
