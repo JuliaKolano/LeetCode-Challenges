5878600d813792ec602e2a852db107fc3c0e905b
JuliaKolano <juliakolano02@gmail.com>
2024-10-02 15:42:32 +0100

    added tests to the solution of the reverse integer problem

---
+++ ReverseInteger/Tests.java
@@ -0,0 +1,13 @@
+package ReverseInteger;
+
+public class Tests {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        
+        System.out.println(solution.reverse(123)); // Answer: 321
+        System.out.println(solution.reverse(-123)); // Answer: -321
+        System.out.println(solution.reverse(120)); // Answer: 21
+    }
+}
