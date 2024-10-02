071942a0b7426a3d41b655f2c213a2b0ff80fb4f
JuliaKolano <juliakolano02@gmail.com>
2024-10-02 14:19:11 +0100

    added the zigzag conversion problem

---
+++ ZigZagConversion/Solution.java
@@ -0,0 +1,13 @@
+package ZigZagConversion;
+
+// The string "PAYPALISHING" is written in a zigzag pattern on a given number of rows like this:
+// (You might want to display this pattern in a fixed font for better legibility)
+// P   A   H   N
+// A P L S I I G
+// Y   I   R
+// And then read line by line: "PAHNAPLSIIGYIR".
+// Write the code that will take a string and make this conversion given a number of rows.
+
+public class Solution {
+
+}
