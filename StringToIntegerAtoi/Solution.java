566676e9a8d7b93a0f24198084099e3ba6794e0f
JuliaKolano <juliakolano02@gmail.com>
2024-10-03 13:30:05 +0100

    added a solution for the string to integer (atoi) problem

--- StringToIntegerAtoi/Solution.java
+++ StringToIntegerAtoi/Solution.java
@@ -12,4 +12,53 @@ package StringToIntegerAtoi;
 
 public class Solution {
 
+    // Time Complexity: O()
+    // Space Complexity: O()
+
+    public int myAtoi(String s) {
+
+        final int length = s.length();
+        int index = 0;
+        int result = 0;
+        boolean isNegative = false;
+
+        if (length == 0) return 0;
+
+
+        // Skipping whitespaces
+        while (index < length && s.charAt(index) == ' ') index++;
+
+        // Handle sign cases
+        if (index < length) {
+
+            if (s.charAt(index) == '-') isNegative = true;
+            else if (s.charAt(index) == '+') index++;
+        }
+
+        // Converting digit (in character form) to interger form
+        // Iterate until non-digit character is found
+        while (index < length && isDigit(s.charAt(index))) {
+
+            // s.charAt(index) - '0' is to convert the char digit into int digit
+            // else it will store the ASCII value of the char digit
+            // so we ASCII of char - ASCII of '0' to get the int digit
+            int digit = s.charAt(index) - '0';
+
+            // To avoid integer overflow
+            if (result > (Integer.MAX_VALUE / 10) || (result == (Integer.MAX_VALUE / 10) && digit > 7)) {
+                return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+
+            // Adding digits at their desired place value
+            result = (result * 10) + digit;
+
+            ++index;
+        }
+
+        return  isNegative ? -result : result;
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
 }
