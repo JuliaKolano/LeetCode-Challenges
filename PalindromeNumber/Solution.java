907317ce3e854fa48bab897addebfa48711b5c79
JuliaKolano <juliakolano02@gmail.com>
2024-10-04 17:34:57 +0100

    added the palindrome number problem

---
+++ PalindromeNumber/Solution.java
@@ -0,0 +1,8 @@
+package PalindromeNumber;
+
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+// Solve it without converting the integer to a string.
+
+public class Solution {
+
+}
