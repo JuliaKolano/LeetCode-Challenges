a878ce9403b00ef29f648cdaa3f1c4e369e35810
JuliaKolano <juliakolano02@gmail.com>
2024-10-05 14:22:17 +0100

    added the regular expression matching problem

---
+++ RegularExpressionMatching/Solution.java
@@ -0,0 +1,10 @@
+package RegularExpressionMatching;
+
+// Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+// '.' matches any single character.
+// '*' matches zero or more of the preceding character.
+// The matching should cover the entire input string (not partial).
+
+public class Solution {
+
+}
