6c0f41214226923d940a969f539d766432929592
JuliaKolano <juliakolano02@gmail.com>
2024-10-07 16:00:36 +0100

    added the container with the most water problem

---
+++ ContainerWithMostWater/Solution.java
@@ -0,0 +1,11 @@
+package ContainerWithMostWater;
+
+// You are given an integer array height of length n. There are vertical lines drawn sucha that the two
+// endpoints of the ith line are (i, 0) and (i, height[i]).
+// Find two lines that together with the x-axis form a container, such that the container contains the most water.
+// Return the maximum zmount of water a container can store.
+// Notice that you may not slant the container.
+
+public class Solution {
+
+}
