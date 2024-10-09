6ca0c37e1400d7a13e333358278962391bba5d27
JuliaKolano <juliakolano02@gmail.com>
2024-10-09 21:24:40 +0100

    added tests to the solution of the integer to roman problem

---
+++ IntegerToRoman/Tests.java
@@ -0,0 +1,13 @@
+package IntegerToRoman;
+
+public class Tests {
+    
+    public static void main(String[] args) {
+        
+        Solution solution = new Solution();
+
+        System.out.println("int = 3749, Answer = " + solution.intToRoman(3749));
+        System.out.println("int = 58, Answer = " + solution.intToRoman(58));
+        System.out.println("int = 1994, Answer = " + solution.intToRoman(1994));
+    }
+}
