1999425f95ecea938e2d60bba24903b07bfb963b
JuliaKolano <juliakolano02@gmail.com>
2024-10-11 11:09:02 +0100

    added the longest common prefix problem

---
+++ LongestCommonPrefix/Solution.java
@@ -0,0 +1,9 @@
+package LongestCommonPrefix;
+
+// Write a function to find the longest common prefix string amongst an array of strings.
+// If there is no common prefix, return an empty string "".
+
+public class Solution {
+
+    
+}
