0688a012b874278309819d7b1cdcd15aa908ec02
JuliaKolano <juliakolano02@gmail.com>
2024-10-11 11:18:21 +0100

    added the horizontal scanning solution to the longest common prefix problem

--- LongestCommonPrefix/Solution.java
+++
@@ -1,9 +0,0 @@
-package LongestCommonPrefix;
-
-// Write a function to find the longest common prefix string amongst an array of strings.
-// If there is no common prefix, return an empty string "".
-
-public class Solution {
-
-    
-}
